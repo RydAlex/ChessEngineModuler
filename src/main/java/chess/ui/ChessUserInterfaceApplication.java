44f86e1a1dd4fbf3614c35dbced24b11afd5b439
aleksanderr <A.Rydzewski@kainos.com>
2017-07-21 00:52:08 +0200

    adding ui module

---
+++ src/main/java/chess/ui/ChessUserInterfaceApplication.java
@@ -0,0 +1,17 @@
+package chess.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class ChessUserInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessUserInterfaceApplication.class, args);
+	}
+}
