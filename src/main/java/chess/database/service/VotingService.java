c0ebc83d53a7468abedd0308b0319e9c44aa214f
aleksanderr <A.Rydzewski@kainos.com>
2017-09-25 00:00:56 +0200

    BlackSheep vs Stockfish

--- src/main/java/chess/database/service/VotingService.java
+++ src/main/java/chess/database/service/VotingService.java
@@ -29,8 +29,8 @@ public class VotingService {
 				temp2 = joinNamesIntoClusterName(temp2, name);
 			}
 		}
-		firstClusterName = temp1 + "1_vs_" + temp2+"2";
-		secondClusterName = temp2 + "2_vs_" + temp1+"1";
+		firstClusterName = temp1 + "_vss_" + temp2;
+		secondClusterName = temp2 + "_vss_" + temp1;
 
 		String suffix = extractSufix(answer);
 		firstClusterName += suffix;
--- src/main/java/chess/manager/game/GameManager.java
+++ src/main/java/chess/manager/game/GameManager.java
@@ -24,11 +24,11 @@ public class GameManager {
     public static void main(String[] argv){
         //Learning manager
         FullInsideGameDefiner gameDefiner = new FullInsideGameDefiner();
-        while(true){
+//        while(true){
             doClusterVsSingleEngineBattles(gameDefiner);
 //            doLearningCallWithEloTypeAndFightOneVsOne(gameDefiner);
 //            doLearningCallForClusters(gameDefiner);
-        }
+//        }
     }
 
 
