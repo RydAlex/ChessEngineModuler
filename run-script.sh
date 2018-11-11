5df5774078f47e9b9fa3e97e93262f3fddd0c4e9
root <RydzewskiAleksander@gmail.com>
2018-11-11 17:31:49 +0000

    cycle-running script

---
+++ run-script.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+~/sbt/bin/sbt -mem 3000 -Djline.terminal=jline.UnsupportedTerminal run </dev/null >output.txt &
+pid=$!
+echo $pid
+
+while :
+do   
+   eval pid_exist=$(ps -o pid= -p $pid)
+   echo $pid_exist
+   if [ -z $pid_exist ]: 
+   then
+      echo "Server 'chessClient' crashed with exit code $?. Respawning..." >&2
+      ~/sbt/bin/sbt -mem 3000 -Djline.terminal=jline.UnsupportedTerminal run </dev/null >output.txt &
+      pid=$!
+      echo $pid
+      sleep 1
+   else
+      date +%D/%T
+      echo "Nothing break"
+      sleep 2 
+   fi
+done
